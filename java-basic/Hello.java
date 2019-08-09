96238d2f89c2e4b4d293490b2fa860377fb016ad
paindor <paindorrr@gmail.com>
2019-08-09 15:43:27 +0900

    190809 15:40

--- java-basic/Hello.java
+++ java-basic/Hello.java
@@ -2,7 +2,7 @@ class Hello{
 
     public static void main(String[] args)
     {
-        System.out.print("Hello ÀÚ¹Ù");
+        System.out.print("Hello JAVA");
         
     }
 }
---
+++ java-basic/WhatName.java
@@ -0,0 +1,9 @@
+class WhatName{
+
+    public static void main(String[] args)
+    {
+        System.out.print("What name");
+
+        
+    }
+}
