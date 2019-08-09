b778b56aa910c2dd6cb8ce456b6cceca452ba854
paindor <paindorrr@gmail.com>
2019-08-09 18:11:00 +0900

    190809 18:10

--- java-basic/WhatName.java
+++ java-basic/WhatName.java
@@ -1,8 +1,25 @@
+import java.util.Scanner;
+
 class WhatName{
 
     public static void main(String[] args)
     {
-        System.out.print("What name");
+        System.out.println("이름뭐야?");
+        String name = "";
+
+        
+        //값입력
+        Scanner scanner = new Scanner(System.in);
+        name = scanner.next();
+       
+        System.out.println("이름은 " + name +  "이다");
+        System.out.print("몇살이야?");
+        int age = 0;
+        age = scanner.nextInt();
+        System.out.print(age + "살 이야");
+
+
+
 
         
     }
