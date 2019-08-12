85bfeb5193cd62e972507a2c9eaeb8625fbcc813
paindor <paindorrr@gmail.com>
2019-08-12 11:59:04 +0900

    190812 11:57

---
+++ java-basic/Calc.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+class Calc{
+    
+    public static void main(String[] args){
+
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("숫자입력>");
+        int input1 = scan.nextInt();
+
+        System.out.print("숫자입력>");
+        int input2 = scan.nextInt();
+
+        int sum = input1 + input2;
+
+        System.out.println(sum);
+
+
+    }
+}
